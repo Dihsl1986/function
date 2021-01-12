3b33d4b7c943e4c9b9f5f9945a7a7138d16bee27
Diego <dihsl1986@gmail.com>
2021-01-11 21:18:11 -0300

    Add files via upload

---
+++ funtion4.js
@@ -0,0 +1,26 @@
+function imprimirNoConsole() {
+    console.log("O´la Mundo!!!!");
+}
+
+imprimirNoConsole();
+
+console.log("-------------------")
+
+function imprimirNumero(num) {
+    console.log("O número é: " + num);
+}
+
+imprimirNumero(89);
+imprimirNumero(9);
+imprimirNumero(62);
+imprimirNumero(812);
+
+console.log("-------------------")
+
+const numeroAleatorio = function () {
+    console.log(Math.random());
+}
+
+numeroAleatorio();
+numeroAleatorio();
+numeroAleatorio();
