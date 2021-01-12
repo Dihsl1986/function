e6f0eb365cc816556260695b83d8a7cc06ca288b
Diego <dihsl1986@gmail.com>
2021-01-11 22:03:36 -0300

    Function soma, dividir, mulitplicar.

---
+++ funtion6.js
@@ -0,0 +1,12 @@
+// SOma , Dividir, Multiplicação
+
+function soma (x,y) {
+    return x + y;
+}
+
+function dividir (x,y) {
+    return x / y;
+}
+
+console.log(soma (5 , 32) );
+console.log(dividir (300 , 30) );
